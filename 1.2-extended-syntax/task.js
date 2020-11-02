2cf1b1a71756b62196abefc0272d5405022ea4c0
Aleksey Denisov <deoz@yandex.ru>
2020-11-02 17:27:58 +0300

    1.2 Task Solved

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,48 @@
+"use sctrict";
+
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let discriminant = Math.pow(b,2) - 4 * a * c;
+    let roots = [];
+    
+    if (discriminant >= 0) {
+        roots.push(( -b + Math.sqrt(discriminant) ) / ( 2 * a ));
+    }
+    if (discriminant > 0) {
+        roots.push(( -b - Math.sqrt(discriminant) ) / ( 2 * a ));
+    }
+
+    return roots;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let arrL = marks.length;
+    let sumMark = 0;
+    
+    if (arrL == 0) {
+        return 0;
+    }
+    
+    if (arrL > 5) {
+        marks = marks.slice(0,5);
+        arrL = 5;
+        console.log("Оценок больше пяти!");
+    }
+    
+    for (i = 0; i < arrL; i++) {
+        sumMark += marks[i];
+    }
+    
+    return sumMark / arrL;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    birthdayYear = dateOfBirthday.getFullYear();
+    currentYear = new Date().getFullYear();
+    
+    if ((currentYear - birthdayYear) >= 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
 }
