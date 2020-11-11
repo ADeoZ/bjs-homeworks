ce1d9bed1477e74ef8a79db2a078f3a757d6765e
Aleksey Denisov <deoz@yandex.ru>
2020-11-11 18:33:07 +0300

    2.2 Task Solved

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,41 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    const text = this.toLowerCase().split(" ").join("");
+
+    for (i = 0; i < text.length / 2; i++) {
+        if (text[i] != text[text.length - i - 1]) {
+            return false;
+        }
+    }
+    
+    return true;    
+}
+
+
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if(!marks.length) {
+        return 0;
+    }
+    
+    let sum = 0;
+    
+    for(i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    
+    const average = sum / marks.length; //не знаю зачем лишние переменные, но так написано в ДЗ
+    const roundedAverage = Math.round(average);
+    
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = Number(new Date);
+    birthday = new Date(birthday); //обычно мы не переназначаем аргументы функции, но так написано в ДЗ
+    
+    const diff = now - birthday;
+    const age = diff / (1000 * 60 * 60 * 24 * 365.25); //високосный год учёл как среднее 365.25, иначе вычисления лучше вообще делать не через Timestamp
+    
+    return (age > 18);
 }
